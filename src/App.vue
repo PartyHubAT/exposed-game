fc1040ea3daaa2856d72ef054b0732f576834976
lukasganster <lukasganster13@gmail.com>
2022-02-11 09:25:40 +0100

    Add background gradient animation

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,15 @@ body {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-
+/*
   background: linear-gradient(to right,
     var(--background-gradient-one),
     var(--background-gradient-two)
   );
+*/
+  background: linear-gradient(-45deg, #080808, #2d2d2d, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+	height: 100vh;
 }
 </style>
