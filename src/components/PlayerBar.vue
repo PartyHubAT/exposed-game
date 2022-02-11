ea76c8d3aca5a45c56b4fc40e45c70bd58d8602e
lukasganster <lukasganster13@gmail.com>
2022-02-11 20:17:45 +0100

    Create PlayerBar.vue

---
+++ src/components/PlayerBar.vue
@@ -0,0 +1,32 @@
+﻿<template>
+  <div id="infoBar">
+    <span v-for="player in $root.players" :key="player">
+      {{ player._id }}
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "PlayerBar",
+  props: {
+    players: {
+      type: Object,
+      required: true
+    }
+  },
+  sockets: {}
+};
+</script>
+
+<style scoped>
+#infoBar {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: beige;
+}
+#infoBar span {
+  margin: 0 5px;
+  font-size: 0.5em;
+}
+</style>
