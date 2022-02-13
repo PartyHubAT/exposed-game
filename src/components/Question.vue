db11d263ce951b39534f20ff9479444e2266371d
lukasganster <lukasganster13@gmail.com>
2022-02-13 15:03:39 +0100

    Update Question.vue

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,16 +1,36 @@
 ﻿<template>
-  <span class="question">{{ question.question }}</span>
-  <span v-if="countAlreadyVoted > 0"
-    >{{ countAlreadyVoted }} already voted!</span
-  >
-  <span class="countdown">{{ countdown }}</span>
-  <div class="voteButtons">
-    <button @click="vote(question.match.playerA)" class="redShadow">
-      {{ question.match.playerA._id }}
-    </button>
-    <button @click="vote(question.match.playerB)" class="greenShadow">
-      {{ question.match.playerB._id }}
-    </button>
+  <div v-if="question">
+    <div
+      class="countdown"
+      :class="{ countdownWarn: votingTime <= 5 && votingTime != 0 }"
+    >
+      {{ votingTime }}
+    </div>
+    <div class="votingInfo">
+      <span v-if="countAlreadyVoted > 0"
+        >{{ countAlreadyVoted }} already voted!</span
+      >
+      <span v-if="countAlreadyVoted == 0">Nobody has voted yet!</span>
+    </div>
+    <span class="question">{{ question.question }}</span>
+    <div class="voteButtons">
+      <button
+        @click="selectVote(question.match.playerA)"
+        v-bind:class="{ selected: selectedVote === question.match.playerA }"
+      >
+        {{ question.match.playerA.name }}
+      </button>
+      <button
+        @click="selectVote(question.match.playerB)"
+        v-bind:class="{ selected: selectedVote === question.match.playerB }"
+      >
+        {{ question.match.playerB.name }}
+      </button>
+      <button class="confirm" @click="confirmVote()" :disabled="alreadyVoted">
+        <span v-if="alreadyVoted">You already voted</span>
+        <span v-if="!alreadyVoted">Confirm</span>
+      </button>
+    </div>
   </div>
 </template>
 <script>
@@ -20,33 +40,40 @@ export default {
     question: {
       type: Object,
       required: true
-    },
-    alreadyVoted: {
-      type: Boolean,
-      default: false
     }
   },
   data: function () {
-    return { countAlreadyVoted: 0, countdown: 30 };
-  },
-  mounted() {
-    setInterval(() => {
-      if (this.countdown != 0) this.countdown--;
-      if (this.countdown == 0) this.voteEnd();
-    }, 1000);
+    return {
+      countAlreadyVoted: 0,
+      votingTime: 10,
+      selectedVote: null,
+      alreadyVoted: false
+    };
   },
   methods: {
-    vote(player) {
+    init() {
+      this.countAlreadyVoted = 0;
+      this.votingTime = this.$root.settings.votingTime;
+      this.selectedVote = null;
+      this.alreadyVoted = false;
+      let interval = setInterval(() => {
+        if (this.votingTime != 0) this.votingTime--;
+        if (this.votingTime == 0) clearInterval(interval);
+      }, 1000);
+    },
+    selectVote(player) {
+      if (this.alreadyVoted) return;
+      this.selectedVote = player;
+    },
+    confirmVote() {
+      if (this.selectedVote === null) return;
+      this.alreadyVoted = true;
+      const player = this.selectedVote;
       const voteObject = {
         index: this.question.index,
-        voteFor: player._id,
         isPlayerA: player._id === this.question.match.playerA._id ? true : false
       };
-      console.log("vote " + JSON.stringify(voteObject));
       this.$socket.emit("voteQuestion", voteObject);
-    },
-    voteEnd() {
-      this.$socket.emit("voteEnd", {});
     }
   },
   sockets: {
@@ -62,25 +89,83 @@ export default {
   color: white;
   font-size: 1.5em;
   padding: 20px;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-style: italic;
+  font-family: var(--font-header);
 }
 
 .voteButtons {
-  display: flex;
+  display: grid;
   gap: 20px;
+  min-width: 300px;
 }
 
 .voteButtons button {
   background-color: black;
   border: 0;
   padding: 10px;
-  box-shadow: 5px 5px 1px rgb(231, 71, 71);
+  box-shadow: 5px 5px 1px var(--primary-dark);
   color: white;
+  transition: 0.3s ease-in-out;
+  border-radius: 5px;
 }
 
-.voteButtons .greenShadow {
-  box-shadow: 5px 5px 1px rgb(71, 231, 140);
+.voteButtons button.selected {
+  box-shadow: 5px 5px 1px var(--secondary);
+  transform: scale(1.1);
+}
+
+.voteButtons .confirm {
+  grid-column-start: 1;
+  grid-column-end: 3;
+}
+
+.voteButtons .confirm:disabled {
+  color: darkslategray;
+}
+
+.countdown {
+  width: 50px;
+  height: 50px;
+  background: var(--secondary);
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  border-radius: 25px;
+  font-size: 1.5em;
+  position: absolute;
+  color: var(--dark);
+  top: 20px;
+  right: 20px;
+  transition: 0.3s ease-in-out;
+  animation-name: countdownPulse;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  font-family: var(--font-header);
+}
+
+.countdownWarn {
+  background: red;
+  animation-name: countdownPulseWarn;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes countdownPulse {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.05);
+  }
+}
+
+@keyframes countdownPulseWarn {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.2);
+  }
 }
 
 /* Extra small devices (phones, 600px and down) */
