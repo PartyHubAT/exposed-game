c50fdb1a89d6812f8ba8688b2ef4d1ee8bdb123e
lukasganster <lukasganster13@gmail.com>
2022-02-20 13:40:42 +0100

    Update info.js

--- public/info.js
+++ public/info.js
@@ -1,7 +1,8 @@
 ﻿module.exports = {
   name: "exposed",
   displayName: "Exposed!",
-  description: "A game about exposing your friends and foes.",
+  description:
+    "Who's Most Likely To? Exposed is a game about exposing your friends and foes. Find out what your friends think about you and who they think is most likely to do absurd things.",
   minPlayerCount: 2,
   maxPlayerCount: 10,
-}
+};
